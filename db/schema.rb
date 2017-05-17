a9a0a6ca866d3a56d9d1445fbf2fcd074b2090a5
zetavg <mail@zeta.vg>
2017-05-17 23:19:19 +0800

    rails generate model Order product_name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517145203) do
+ActiveRecord::Schema.define(version: 20170517151842) do
+
+  create_table "orders", force: :cascade do |t|
+    t.string "product_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
